cbc670d9d4d2233556320133f8be9f2c2ec16f20
gfelix <gfelix@sp.senai.br>
2024-08-22 10:30:27 -0300

    feat: Add initial project files and structure
    This commit adds the initial project files and structure. It includes the following changes:
    
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    - Added  file
    
    These changes lay the foundation for the project and set up the necessary files and structure for further development.

---
+++ lib/istic_ui_mobile.dart
@@ -0,0 +1,6 @@
+library istic_ui_mobile;
+
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/widgets/button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:istic_ui_mobile/enums/button_type.dart';
+import 'package:istic_ui_mobile/extensions/context_extension.dart';
+
+class IstButton extends StatelessWidget {
+  final ButtonType buttonType;
+  final String? title;
+  final Widget? customTitle;
+  final VoidCallback? action;
+
+  const IstButton({
+    super.key,
+    this.buttonType = ButtonType.primary,
+    this.title,
+    this.customTitle,
+    this.action,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = context.theme.fontStyle.bodyMedium;
+
+    return GestureDetector(
+      onTap: action,
+      child: Container(
+        width: 200,
+        height: 50,
+        color: buttonType.color,
+        child: customTitle ?? Text(
+          title!,
+          style: textStyle
+        )
+      ),
+    );
+  }
+}
